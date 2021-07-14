92cceb5de314ac13c95b6bdad5830d61455816d5
sanjaykiranch <sanjusanjaykiran890@gmail.com>
2021-07-14 21:55:47 +0530

    TicTacToe App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Icon from "./Components/Icon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ToastContainer , toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+ 
+import { Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./style.css";
+ 
+const myArray = new Array(9).fill("");
+const App=()=>{
+    let [isCross,setIsCross] = useState(true)
+    let [winMessage,setWinMessage]=useState("")
+
+    const playAgain=()=>{
+        setIsCross(true)
+        setWinMessage("")
+        myArray.fill("")
+    }
+
+    const findWinner=()=>{
+        if(myArray[0]===myArray[1] && myArray[1]===myArray[2]){
+            setWinMessage("winner");
+        }
+        if(myArray[3]===myArray[4] && myArray[4]===myArray[5]){
+            setWinMessage("winner");
+        }
+        if(myArray[6]===myArray[7] && myArray[7]===myArray[8]){
+            setWinMessage("winner");
+        }
+        if(myArray[0]===myArray[4] && myArray[4]===myArray[8]){
+            setWinMessage("winner");
+        }
+        if(myArray[2]===myArray[4] && myArray[4]===myArray[6]){
+            setWinMessage("winner");
+        }
+        if(myArray[0]===myArray[3] && myArray[3]===myArray[6]){
+            setWinMessage("winner");
+        }
+        if(myArray[1]===myArray[4] && myArray[4]===myArray[7]){
+            setWinMessage("winner");
+        }
+        if(myArray[2]===myArray[5] && myArray[5]===myArray[8]){
+            setWinMessage("winner");
+        }
+        if(myArray[2]===myArray[5] && myArray[5]===myArray[8]){
+            setWinMessage("winner");
+        }
+    }
+
+
+    return(
+        <div>
+            <Icon choice=""/>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Components/Icon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {FaTimes, FaRegCircle, FaPen } from "react-icons/fa";
+
+const Icon=({choice})=>{
+    switch(choice){
+        case "cross":
+            return(<FaTimes />)
+        case "circle":
+            return(<FaRegCircle />)
+        default:
+            return(<FaPen />)
+    }
+}
+
+export default Icon;
